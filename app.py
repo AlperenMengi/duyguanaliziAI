de37cb7cdb058dc02684456abe9b3a3cd776ddae
Alperen Mengi <alprnmengi97@gmail.com>
2023-11-14 23:21:26 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from tespit import deneme
+from keras.preprocessing import image
+from keras.models import load_model
+import numpy as np
+from flask_cors import CORS
+from flask import Flask, make_response, jsonify
+
+app = Flask(__name__)
+CORS(app)
+
+print("DENEME -------- ")
+
+
+@app.route('/start', methods=['POST'])
+def start():
+        result = deneme()
+        response = jsonify(result)
+        return response
+
+
+@app.route('/test', methods=['GET'])
+def start2():
+    return jsonify({"message":"bu bir get method"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
